d194c2dbf57689a759748d6c67bd77f90432cee0
Shishi <andreiusita6@gmail.com>
2025-10-24 15:36:35 +0800

    Add Book constructor for book creation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+let myLibrary = [];
+
+function Book(id, name, author, page, read) {
+	// Book constructor will be used on creating books.
+	this.id = id;
+	this.name = name;
+	this.author = author;
+	this.page = page;
+	this.read = read;
+}
+
+let object1 = new Book(1, 'Luminae', 'Shishi Hiiragi', '123', 'read');
+let object2 = new Book(2, 'Verdants', 'Shishi Hiiragi', '145', 'read');
+
 function init() {
 	// Initialize event listeners
 	const container = document.querySelector('.container');
@@ -9,9 +23,9 @@ function getClick(e) {
 
 	// Modal related conditions
 	if (e.target.id === 'showBookModal') {
-		return showBookModal();
+		showBookModal();
 	} else if (e.target.id === 'closeBookModal') {
-		return closeBookModal();
+		closeBookModal();
 	}
 	console.log(e.target);
 }
@@ -20,13 +34,12 @@ function showBookModal() {
 	// This will show the modal for the book
 	const modal = document.getElementById('addBookModal');
 	modal.showModal();
-	return;
 }
 
 function closeBookModal() {
+	// This function will close the modal
 	const modal = document.getElementById('addBookModal');
 	modal.close();
-	return;
-} 
+}
 
 init();
